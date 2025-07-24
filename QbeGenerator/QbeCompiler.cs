12fe068d7f1ec1192899417c49f4bf852d0c9db1
Marten Rus <martinrages@gmail.com>
2025-07-24 17:23:11 +0200

    aaaaaaaaaaaaaaaaaa

--- QbeGenerator/QbeCompiler.cs
+++ QbeGenerator/QbeCompiler.cs
@@ -4,50 +4,14 @@ namespace QbeGenerator;
 
 public static class QbeCompiler
 {
-    public static bool IsExecutableInPath(string fileName)
-    {
-        try
-        {
-            // Run the process to check if the executable is available in the PATH
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = fileName,
-                    Arguments = "-h", // Just a dummy argument to check if it runs
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                }
-            };
-            
-            process.Start();
-            process.WaitForExit();
-
-            return true;
-        }
-        catch (Exception)
-        {
-            // If we can't start the process, it means the executable is not found
-            return false;
-        }
-
-        return false;
-    }
-    
     /// <summary>
     /// Compiles the given QBE IR to assembly code.
     /// Which can be used by Clang or other assemblers to produce a binary.
     /// </summary>
     public static bool Compile(string qbeIr, out string assembly, out string? qbeError)
     {
-        if (!IsExecutableInPath("qbe"))
-        {
-            assembly = string.Empty;
-            qbeError = "QBE compiler not found in PATH. Please install QBE and ensure it is accessible.";
-            return false; // QBE compiler not found
-        }
+        // yk what i dont even care anymore no more checking if it exists
+
         var tempFile = Path.GetTempFileName();
         var asmOut = Path.Combine(Path.GetTempPath(), "output.s");
         try
